cd776ff03447aafdf5143bea1eff8ac189a7003c
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-12 14:40:47 +0800

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,24 +1,24 @@
 import requests, os
 
 def cekip():
-print(f'[!] Mendapatkan IP..')
-re = requests.get('https://api.myip.com').json()
-ip = re['ip']
-print(f'[!] IP kamu : {ip}')
-
+ print(f'[!] Mendapatkan IP..')
+ re = requests.get('https://api.myip.com').json()
+ ip = re['ip']
+ print(f'[!] IP kamu : {ip}')
+ 
 def iOsif():
-print(f'[!] Menginstall tools osif..')
-os.system('pkg update upgrade')
-os.system('pkg install git python2')
-os.system('git clone https://github.com/ciku370/OSIF')
-os.system('cd OSIF')
-os.system('pip2 install -r requirements.txt')
-os.system('python2 osif.py')
+ print(f'[!] Menginstall tools osif..')
+ os.system('pkg update upgrade')
+ os.system('pkg install git python2')
+ os.system('git clone https://github.com/ciku370/OSIF')
+ os.system('cd OSIF')
+ os.system('pip2 install -r requirements.txt')
+ os.system('python2 osif.py')
+ 
 
+print('''-=[ MyTools ]=-
 
-print('''-=[ MR.404 ]=-
-
-[Menu]
+     [Menu]
 [1] Cek IP
 [2] Install OSIF
 [3] Keluar
@@ -26,12 +26,12 @@ print('''-=[ MR.404 ]=-
 menu = input('[?] Silahkan pilih menu : ')
 
 if menu == '1':
-cekip()
+ cekip()
 elif menu == '2':
-iOsif()
+ iOsif()
 elif menu == '3':
-print('[?] Keluar..')
-sys.exit()
+ print('[?] Keluar..')
+ sys.exit()
 else:
-print('[?] Perintah tidak diketahui..')
-sys.exit()
+ print('[?] Perintah tidak diketahui..')
+ sys.exit()
